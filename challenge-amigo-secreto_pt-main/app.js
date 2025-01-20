98b5a7c68ace5100ad07ee70ef3c68a43d86f1e7
Santiago Henrique <santiago.henrique@ufrpe.br>
2025-01-20 13:20:54 -0300

    Implementação do desafio

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,35 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let nomes = [];
+
+function adicionarAmigo() {
+  let inputNome = document.getElementById('amigo');
+  let nome = inputNome.value;
+  if (nomes.includes(nome) === false && nome !== '') {
+    nomes.push(nome);
+    inputNome.value = '';
+    atualizarLista();
+  } else if (nomes.includes(nome)) {
+    alert('Esse nome já foi adicionado!');
+  } else if (nome === '') {
+    alert('Por favor, Digite um nome!');
+    }
+  }
+
+function atualizarLista() {
+  lista = document.getElementById('listaAmigos');
+  lista.innerHTML = '';
+  nomes.forEach((nome) => {
+    let item = document.createElement('li');
+    item.textContent = nome;
+    lista.appendChild(item);
+  });
+}
+
+function sortearAmigo() {
+  let indiceDaLista = Math.floor(Math.random() * nomes.length);
+  let amigoSorteado = nomes[indiceDaLista];
+  alert(`O amigo sorteado é: ${amigoSorteado}`);
+  if (amigoSorteado) {
+    nomes.splice(indiceDaLista, 1);
+    atualizarLista();
+  }
+}
